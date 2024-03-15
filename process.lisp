3ff7ee9765eb64bde1455e9cc7b7b1f22a4cde6f
Wade Mealing <wmealing@gmail.com>
2024-03-16 02:33:28 +1000

    Working on it...

--- process.lisp
+++ process.lisp
@@ -13,22 +13,84 @@
 (defvar SOX-END "F7")
 
 
+(defparameter header-start 0)
+(defparameter header-len 4)
+(defparameter header-end (+ header-start header-len))
+
+(defparameter header-padding-start header-end)
+(defparameter header-padding-len 2)
+(defparameter header-padding-end (+ header-padding-start header-padding-len))
+
+(defparameter direction-start header-padding-end)
+(defparameter direction-len 1 )
+(defparameter direction-end (+ direction-start direction-len))
+
+(defparameter seq-num-start direction-end)
+(defparameter seq-num-len 1 )
+(defparameter seq-num-end (+ seq-num-start seq-num-len))
+
+
+;; FIX
+(defparameter rest seq-num-end)
+
+(defvar message-count 0)
+
+(defun pack (source-array)
+  (let ((r 1)
+        (i 0)
+        (target-array  (make-array (length source-array))))
+    (loop for s below (length source-array)
+          do (let ((o (mod s 7))
+                   (l (ash (aref source-array s) -7)))
+               (setf (aref target-array i) (logior (aref target-array i) (ash l o)))
+               (setf (aref target-array r) (logand (aref source-array s) 127))
+               (when (= o 6)
+                 (when (< s (- (length source-array) 1))
+                   (incf i 8)
+                   (incf r)))))
+    target-array))
+
 (defun process-line (line)
+  (check-type line string)
 
-  (format t "Packet #N - ")
+  (format t "Midi Message #~s ~%" message-count)
 
-  (let ((table (ascii-table:make-table '("Data" "Description") :header "Packet #x"))
-        (sp  (split-sequence:split-sequence  #\Space line)))
+  (setq message-count (+ message-count 1))
+  
+  ;;  (format t "LINE LENGTH: ~s~%" (length line))
+  
+  (if (>= 18 (length line))
+      (progn
+        (format t "LINE TOO SHORT ~%")
+        (force-output)
+        (return-from process-line nil)))
 
-    (trivia:match sp
-      ((cons x y)
-       (format t "X: ~s~%" x)
-       (format t "Y: ~s~%" y)))
+  (let* (;;(table (ascii-table:make-table '("Data" "Description") :header "Packet #x"))
+         (line-trimmed   (string-left-trim '(#\Space #\Tab #\Newline)  line))
+         (final-seq      (split-sequence:split-sequence  #\Space line-trimmed))
+         (header         (subseq final-seq header-start header-end))
+         (header-padding (subseq final-seq header-padding-start header-padding-end))
+         (direction (subseq final-seq direction-start direction-end))
+         (seq-number (subseq final-seq seq-num-start seq-num-end))
+         ;;         (mmmm (subseq final-seq header-padding-end))
+         )
+
+    (format t "FINAL-SEQ LENGTH: ~s~%" (length final-seq))
+    ;;    (format t "SEQ: ~s~%" final-seq)
+    (format t "HEADER: ~s~%" header)
+    (format t "PADDING: ~s~%" header-padding)
+    ;;    (format t "DIRECTION-START: ~s~%" direction-start)
+    ;;    (format t "DIRECTION-END: ~s~%" direction-end)
+    (format t "DIRECTION: ~s~%"   direction)
+    (format t "SEQ-NUM: ~s~%"   seq-number)
+    (format t "----------~%")
+
 
     ;;    (ascii-table:add-row table '(1 "Bob" 150))
     ;;    (ascii-table:add-separator table)
     ;;    (ascii-table:add-row table '("" "Total" 350))
-    (ascii-table:display table)))
+    ;; (ascii-table:display table)
+    ))
 
 
 (defun start ()
@@ -36,9 +98,7 @@
     (when in
       (loop for line = (read-line in nil)
             while line do (progn
-                            (let* ((s1 (str:split "bytes"  line))
-                                   (s2 (str:substring 4 t s1)))
-                              ;;                            (format t "LINE: ~a~%" (first s2))
+                            (let* ((s1 (str:split "bytes"  line)))
                               (process-line (second s1))
                               )))
       (close in))))
